ea6a5681d121e3dd32af7d2f698440e84696220a
Charlie <N1046401@ntub.edu.tw>
2020-03-05 10:09:51 +0800

    fixSomeError(stillbeta)

--- discretefft.py
+++ discretefft.py
@@ -1,26 +1,28 @@
 import numpy.fft as nf
 import numpy as np
 from moviepy.editor import *
-
 def takeLoud(video_clip):
 	audio = video_clip.audio
 	x = audio.to_soundarray(fps=44100)
 	sample_rate = 44100
 	times = np.arange(len(x)) / sample_rate
 	print(times)
-	freqs = nf.fftfreq(times.size,1/sample_rate)
+	freqs = nf.fftfreq(times.size,1 / sample_rate)
 	noised_ffts = nf.fft(x)
 	noised_pows = np.abs(noised_ffts)
-	fund_freq = np.abs(freqs[noised_pows.argmax()])
-	quit_indices = np.where(np.abs(freqs)==fund_freq)
-	quit_pows = x[quit_indices]
-	quit_times = times[quit_indices]
-	VideoClip = []
-	AudioClip = []
+	quit_indices = np.where(noised_pows > np.mean(noised_pows)*10)
+	quit_pows = x[quit_indices[0]]
+	quit_times = times[quit_indices[0]]
+	timeList = []
+	VideoClip,AudioClip = [],[]
 	for i in range(len(quit_times)-1):
-		VideoClip.append(video_clip.subclip(quit_times[i],quit_times[i+1]))
-		AudioClip.append(audio.subclip(quit_times[i],quit_times[i+1]))
-
+		if quit_times[i+1] - quit_times[i] < 1:
+			continue
+		timeList.append(quit_times[i])
+	for i in range(len(timeList)-1):
+		VideoClip.append(video_clip.subclip(timeList[i],timeList[i+1]))
+		AudioClip.append(audio.subclip(timeList[i],timeList[i+1]))
+		print("append complete")	
 	concateVideo = concatenate_videoclips(VideoClip)
 	concateAudio = concatenate_audioclips(AudioClip)
 
